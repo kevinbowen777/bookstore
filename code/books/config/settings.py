547806a4c7c5e99a587a3b7446e69bb5c7593404
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-07 22:42:12 -0700

    Clean up email relay config

--- code/books/config/settings.py
+++ code/books/config/settings.py
@@ -140,7 +140,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 ACCOUNT_USERNAME_REQUIRED = False
@@ -148,7 +147,7 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-# DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+# third-party email relay configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'kevin.bowen@gmail.com'
