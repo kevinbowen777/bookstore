6eaf6d00a7f86492a5a4733794e945de87092b86
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-08 18:51:25 -0700

    Create book_list & book_detail tests

--- code/books/books/tests.py
+++ code/books/books/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+
+class BookTests(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='Learning Python',
+            author='Mark Lutz',
+            price='53.00',
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'Learning Python')
+        self.assertEqual(f'{self.book.author}', 'Mark Lutz')
+        self.assertEqual(f'{self.book.price}', '53.00')
+
+    def test_book_list(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Learning Python')
+        self.assertTemplateUsed(response, 'books/book_list.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Learning Python')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
