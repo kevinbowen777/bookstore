7deb52ec03146dcd3387d55912998224fdc03a8f
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-09 14:03:20 -0700

    Update for static media

--- code/books/config/urls.py
+++ code/books/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -11,4 +13,4 @@ urlpatterns = [
     # Local applications
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
