d42fabb7c102ae3c4b7ebd5c76645cb377fd998f
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-09 18:13:17 -0700

    Create queryset using Q object

--- code/books/books/views.py
+++ code/books/books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 # PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
@@ -24,3 +25,8 @@ class SearchResultsListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
