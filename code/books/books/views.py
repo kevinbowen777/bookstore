2aa951678b92a6d8981284c0dadd427f51ddde24
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-11 02:51:42 -0700

    Remove references to special restricted permissions

--- code/books/books/views.py
+++ code/books/books/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
-# PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 
@@ -13,12 +12,10 @@ class BookListView(LoginRequiredMixin, ListView):
 
 
 class BookDetailView(LoginRequiredMixin, DetailView):
-    # PermissionRequiredMixin,
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
-    # permission_required = 'books.special_status'
 
 
 class SearchResultsListView(ListView):
