3002a33e5ddfe8603018eacd4cd48d32a1d4c82b
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-13 19:26:38 -0700

    Wire add URL BookCreateView

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import BookDetailView, BookListView, SearchResultsListView
+from .views import (
+    BookCreateView,
+    BookDetailView,
+    BookListView,
+    SearchResultsListView,
+)
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
+    path("add/", BookCreateView.as_view(), name="book_add"),
     path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
     path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
