6af1fbec4ac51e98ed08967cda749abd1da91fac
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-14 11:13:41 -0700

    Wire in UpdateView URL pattern

--- books/urls.py
+++ books/urls.py
@@ -4,12 +4,14 @@ from .views import (
     BookCreateView,
     BookDetailView,
     BookListView,
+    BookUpdateView,
     SearchResultsListView,
 )
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
     path("add/", BookCreateView.as_view(), name="book_add"),
+    path("<uuid:pk>/update/", BookUpdateView.as_view(), name="update"),
     path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
     path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
