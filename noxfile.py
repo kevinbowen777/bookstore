36c6c7b74593c1181bf41db4c14d89f2aa9e1be4
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-25 17:12:32 -0700

    Configure flake8 nox session

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,15 @@
 """Nox sessions."""
 import nox
 
+locations = "accounts", "books", "config", "pages", "noxfile.py"
+
+
+@nox.session(python=["3.10", "3.9"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
 
 @nox.session(python=["3.10", "3.9"])
 def tests(session):
