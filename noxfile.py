5babfd23efe97884d97ee8fd83970d7c0dc29f04
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-26 09:47:00 -0700

    Implement install_with_constraints function

--- noxfile.py
+++ noxfile.py
@@ -7,11 +7,26 @@ nox.options.sessions = "lint", "safety", "tests"
 locations = "accounts", "books", "config", "pages", "./noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    """Install packages constrained by Poetry's lock file."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.10", "3.9"])
 def black(session):
     """Run black code formatter."""
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -19,7 +34,8 @@ def black(session):
 def lint(session):
     """Lint using flake8."""
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -42,7 +58,7 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run(
             "safety", "check", f"--file={requirements.name}", "--full-report"
         )
@@ -52,5 +68,14 @@ def safety(session):
 def tests(session):
     """Run the test suite."""
     args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session,
+        "coverage[toml]",
+        "django-coverage-plugin",
+        "factory-boy",
+        "pytest",
+        "pytest-cov",
+        "pytest-django",
+    )
     session.run("pytest", *args)
