cf19c99448ea44954b2cd210e47efafcbe3c0ea8
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-26 09:06:15 -0700

    Add safety session

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
 """Nox sessions."""
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 locations = "accounts", "books", "config", "pages", "./noxfile.py"
 
 
@@ -25,6 +27,25 @@ def lint(session):
         "flake8-import-order",
     )
     session.run("flake8", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
+def safety(session):
+    """Scan dependencies for insecure packages."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run(
+            "safety", "check", f"--file={requirements.name}", "--full-report"
+        )
 
 
 @nox.session(python=["3.10", "3.9"])
