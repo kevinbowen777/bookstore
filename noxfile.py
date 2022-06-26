23db30ad735a480ab0d3220965831535b7665dcc
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-25 17:42:22 -0700

    Add session for black

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,16 @@ locations = "accounts", "books", "config", "pages", "noxfile.py"
 
 
 @nox.session(python=["3.10", "3.9"])
+def black(session):
+    """Run black code formatter."""
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.10", "3.9"])
 def lint(session):
+    """Lint using flake8."""
     args = session.posargs or locations
     session.install("flake8")
     session.run("flake8", *args)
@@ -13,6 +22,7 @@ def lint(session):
 
 @nox.session(python=["3.10", "3.9"])
 def tests(session):
+    """Run the test suite."""
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
