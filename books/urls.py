f4b3e34877a446f5d379ef32ad7d4c03ce402979
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-12 14:05:03 -0700

    Fix improperly formatted path

--- books/urls.py
+++ books/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path("add/", BookCreateView.as_view(), name="book_add"),
     path("<uuid:pk>/update/", BookUpdateView.as_view(), name="update"),
     path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
-    path("uuid:pk/review/add/", ReviewCreateView.as_view(), name="review_add"),
+    path(
+        "<uuid:pk>/review/add/", ReviewCreateView.as_view(), name="review_add"
+    ),
     path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
