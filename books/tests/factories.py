1d90a26f2be554376f2b8f76be4c8e7a8d1d6a74
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-15 19:46:34 -0700

    Add default prefix to titles

--- books/tests/factories.py
+++ books/tests/factories.py
@@ -6,7 +6,7 @@ from ..models import Book
 
 
 class BookFactory(factory.django.DjangoModelFactory):
-    title = factory.fuzzy.FuzzyText()
+    title = factory.fuzzy.FuzzyText(length=12, prefix="The Book of ")
     author = factory.SubFactory(UserFactory)
     price = factory.fuzzy.FuzzyDecimal(low=9.95, high=99.99)
     creator = factory.SubFactory(UserFactory)
